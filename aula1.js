bb2f3c5e3eba9cc74ef39d77fcd8533c8c31cea7
Wallace Rangel <wallacerangelgama7@gmail.com>
2022-11-25 10:19:43 -0300

    AulaNode-v1.1

--- aula1.js
+++ aula1.js
@@ -3,6 +3,6 @@ http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type':'test/html'});
     res.write('<h2>RAPHINHA CHUTA FOFO</h2> <br>');
     res.write(Date().substring(16,24)+'<br>');
-    res.end('EIKE GOSTOSO');
+    res.end('EIKE');
 
 }).listen(8080);
