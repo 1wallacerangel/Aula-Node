84e5da4a03dd0cc8f0ddf51dea08caf7830a37f8
Wallace Rangel <wallacerangelgama7@gmail.com>
2022-12-08 08:32:33 -0300

    AulaNode-v1.2

--- aula1.js
+++ aula1.js
@@ -1,10 +1,8 @@
 var http = require('http');
-var dia = require('./bliblioteca');
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type':'test/html'});
-    res.write('<h2> Brasil 2 x 1 Suíca </h2> <br>');
-    res.write('<p style=color:green>'+Date().substring(16,24)+'<br>');
-    res.write(dia.diaDaSemana()+'</p><br>')
-    res.end('EIKE');
+    res.write('<h2> Brasil 2 x 1 Servia </h2> <br>');
+    res.write(Date().substring(16,24)+'<br>');
+    res.end('Fim');
 
 }).listen(8080);
